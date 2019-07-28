cb3e9d4186464d5ca10fbe75b0dbb78886aa5904
Dubstepic <JC@Dubstepics-MacBook-Air.local>
2019-07-28 19:33:01 +0700

    8_kyu_Bin_to_Decimal

---
+++ 8_kyu_Bin_to_Decimal.js
@@ -0,0 +1,5 @@
+/* Complete the function which converts a binary number (given as a string) to a decimal number. */
+/* jshint esversion:6 */
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
