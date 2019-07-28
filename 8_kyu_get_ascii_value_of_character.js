3e9b7951fbf5107b10df6ba7c6389d66ac48a3d3
Dubstepic <JC@Dubstepics-MacBook-Air.local>
2019-07-28 19:38:49 +0700

    8_kyu_get_ascii_value_of_character

---
+++ 8_kyu_get_ascii_value_of_character.js
@@ -0,0 +1,9 @@
+/* Write a function getASCII which inputs a character and returns the corresponding ascii value for that character.
+Example : getASCII('A') => 65 */
+
+// SOLUTION
+/* jshint esversion:6*/
+
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
